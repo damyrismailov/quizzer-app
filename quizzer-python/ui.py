9fbb85cc0d9db6ae67b8f1be1439a398488bc337
Damyr Ismailov <damyrismailov@gmail.com>
2025-12-18 15:53:28 +0300

    Create ui.py

---
+++ quizzer-python/ui.py
@@ -0,0 +1,49 @@
+from tkinter import *
+THEME_COLOR = "#375362"
+from quiz_brain import QuizBrain
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain ):
+        self.quiz_brain = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = Label(self.window,text="Score: ", bg=THEME_COLOR, fg='white')
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(self.window, width=300, height=250, background="White")
+        self.question_text = self.canvas.create_text(150, 125, text="Amazon",
+                                                     fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic",), width=280)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        my_image_yes = PhotoImage(file="images/true.png")
+        my_image_no = PhotoImage(file="images/false.png")
+        self.button_yes = Button(image=my_image_yes, highlightthickness=0, command=self.true_button)
+        self.button_yes.grid(row=2, column=0)
+        self.button_no = Button(image=my_image_no, highlightthickness=0, command=self.false_button)
+        self.button_no.grid(row=2, column=1)
+        self.get_next_question()
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz_brain.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz_brain.score}")
+            q_text = self.quiz_brain.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz")
+            self.button_yes.config(state="disabled")
+            self.button_no.config(state="disabled")
+    def true_button(self):
+        self.give_feedback(self.quiz_brain.check_answer("True"))
+    def false_button(self):
+        self.give_feedback(self.quiz_brain.check_answer("False"))
+
+    def give_feedback(self, is_right ):
+        if is_right:
+            self.canvas.config(bg= "green")
+        else:
+            self.canvas.config(bg = "red")
+        self.window.after(1000, self.get_next_question)
